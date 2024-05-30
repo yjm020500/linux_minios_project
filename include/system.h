aba6064f88a3b6b3199a1afefaa4be90901f6d18
Hyunwoo Kim <aq3480@naver.com>
2024-05-31 01:06:17 +0900

    수정

--- include/system.h
+++ include/system.h
@@ -1,9 +1,9 @@
 // include/linux/sched.h
-//SSU struct task_struct {
+//SSU struct task_struct
 
 void minisystem();
 
-void printWithDelay(const char *str, int delay);
+void printWithDelay(const char *str, int delay); // 한글자씩 출력하는 함수 (in kernel/mem_s/print_util.c)
 void print_minios(char* str);
 
 void * memory_set(void * ptr, int value, size_t num);

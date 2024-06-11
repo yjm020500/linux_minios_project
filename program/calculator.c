51db09a25555d5d28ea406b23ad9f30857e33efd
yjm020500 <162987634+yjm020500@users.noreply.github.com>
2024-06-11 22:20:48 +0900

    Create calculator.c

---
+++ program/calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int chara;
+    int number; int num1; int num2;
+    printf("add=1,sub=2,mul=3,div=4\n");
+    scanf("%d", &chara);
+    printf("Enter first number\n");
+    scanf("%d", &num1);
+    printf("Enter second number\n");
+    scanf("%d", &num2);
+    if(chara==1){
+        number=num1+num2;
+    }
+    else if(chara == 2){
+        number=num1-num2;
+    }
+    else if(chara==3){
+        number=num1*num2;
+    }
+    else if(chara==4){
+        number=num1/num2;
+    }
+    printf("result : %d \n", number);
+}

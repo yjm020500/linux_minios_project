5ee78f1d68e6fb54e45911d60c143a064ffb9700
yjm020500 <162987634+yjm020500@users.noreply.github.com>
2024-06-11 22:26:08 +0900

    4pages

---
+++ program/helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#incldue <stdlib.h>
+
+int mian(void){
+ while(1){
+    int num
+    printf("if you want to start, enter 1\n");
+    scanf("%d", &num);
+    if(num == 1) {break;}
+ }
+printf("hello world!");
+}

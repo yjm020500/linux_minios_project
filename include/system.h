28429879a8b577b768bb67c8febe42841687d588
yjm020500 <162987634+yjm020500@users.noreply.github.com>
2024-06-11 11:24:36 +0900

    processpool 관련 추가

--- include/system.h
+++ include/system.h
@@ -13,5 +13,5 @@ void * make_dummy_physical_memory();
 int prepare_dummy_physical_memory_reorder(void * start_adr, void * end_adr);
 void memory_view(unsigned char *memory, size_t from, size_t to);
 
-void execute(void * virtual_physical_memory, FrameList * fl, FrameManager * fm);
+void execute(void * virtual_physical_memory, FrameList * fl, FrameManager * fm, ProcessPool* pp);
 void terminate();
